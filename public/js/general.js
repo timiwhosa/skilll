c86c72c7b8dba327649eb796057387d37e92f2d3
timiwhosa <64585862+timiwhosa@users.noreply.github.com>
2021-11-06 01:37:55 +0100

    community

--- public/js/general.js
+++ public/js/general.js
@@ -1,268 +1,395 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementsByClassName("search-div")[0]
-    .addEventListener("mouseenter", () => opensearch());
-  document
-    .getElementsByClassName("width")[0]
-    .addEventListener("mouseenter", () => closesearch());
+  // document
+  //   .getElementsByClassName("search-div")[0]
+  //   .addEventListener("mouseenter", () => opensearch());
+  // document
+  //   .getElementsByClassName("width")[0]
+  //   .addEventListener("mouseenter", () => closesearch());
 });
 
-function opensearch() {
-  if (document.documentElement.clientWidth < 482) {
-    var hy = document.getElementsByClassName("float")[0];
-    hy.style.width = "100%";
-  }
-}
-function closesearch() {
-  if (document.documentElement.clientWidth < 482) {
-    var hy = document.getElementsByClassName("float")[0];
-    hy.style.width = "0";
-  }
-}
+// Escape text
+String.prototype.escape = function () {
+  var tagtoreplace = {
+    "&": "&amp;",
+    "<": "&lt",
+    ">": "&gt",
+    "=": "",
+    script: " ",
+    Script: " ",
+    '"': " ",
+    "`": " ",
+  };
+  return this.replace(/[&<>`=]/g, function (tag) {
+    return tagtoreplace[tag] || tag;
+  });
+};
 
 // SEARCH section
-var allInputs = Object.entries(document.getElementsByTagName("input"));
-allInputs.forEach((input) => {
-  if (input[1].type === "search") {
-    input[1].addEventListener("keyup", (e) => {
-      if (e.keyCode === 13) {
-        search(e.target.value);
-        return false;
-    }})
-  }
-})
-
 if (
   (document.getElementsByClassName("search-btn")[0],
-  document.getElementById("searchForm"))
+  document.getElementById("searchForm"),
+  document.getElementById("school"),
+  document.getElementById("join-form"))
 ) {
+  // search input
+  var searchInput = document.getElementById("search");
+  searchInput.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+      search(e.target.value);
+    }
+  });
+  // adding event listener to search-select your school
+  document
+    .getElementById("school")
+    .addEventListener("keyup", saveSelectSchool);
+  // setting select value to localStoragechoosen school
+  var school = JSON.parse(localStorage.getItem("skilll-school"));
+  if (school) {
+    document.getElementById("school").value = school.school;
+  }
+
+  // eventlistener for search form
   document.getElementById("searchForm").addEventListener("submit", (e) => {
     e.preventDefault();
     return false;
   });
+  // eventlistener for search button
   document
     .getElementsByClassName("search-btn")[0]
     .addEventListener("pointerdown", (e) => {
-      search(e.target.parentNode[0].value);
+      search(searchInput.value);
     });
+
+  // Join form
+  var join = document.getElementById("join-form");
+  join.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Join(e);
+    return false;
+  });
 }
 
-function search(e) {
+// save selected school to localStorage
+function saveSelectSchool(e) {
+  document.getElementById("school").classList.remove("redBorder");
+  if (e.target.value.trim().length > 0) {
+    localStorage.setItem(
+      "skilll-school",
+      JSON.stringify({ school: e.target.value.trim() })
+    );
+  }
+}
+
+function search(searchValue) {
+  var school = JSON.parse(localStorage.getItem("skilll-school"));
   // console.log(e)
-  if(e.trim().length > 0 ){
-    fetch(`/search?input=${e}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        if (data.status === 200) {
-          savesearch(data.data);
-          window.location.href = `/search/${e}`;
-        } else {
-          promptDiv(data.message);
-        }
-      });
+  // searchValue = e.target.parentElement.previousElementSibling.children[0].value;
+
+  // get the value for input field lol
+  if (searchValue.trim().length > 0) {
+    if (school && school.school) {
+      fetch(`/search?keyword=${searchValue}&school=${school.school}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          if (data.status === 200) {
+            data.keyword = searchValue
+            savesearch(data);
+            loadList(data.data);
+            console.log(data);
+          } else {
+            messageDiv(data);
+          }
+        });
+    } else {
+      document.getElementById("school").classList.add("redBorder");
+    }
   }
 }
 
-function savesearch(data){
-  sessionStorage.setItem("searchResult", JSON.stringify(data, null,2))
-};
-function promptDiv(data){
+function savesearch(data) {
+  sessionStorage.setItem("SkilllsearchResult", JSON.stringify(data, null, 2));
+}
+function messageDiv(data) {
   var suc = document.createElement("div");
-  suc.setAttribute("class", "message-show bg-green1 white");
-  suc.setAttribute("id", `searchResponse${Math.floor(Math.random())}`);
-  suc.textContent = data;
-  document.body.append(suc);
-  setTimeout(() => {
-    document.body.removeChild(suc);
-  }, 2500);
-};
-
-
-
-
-var products = JSON.parse(localStorage.getItem("honeycareproduct"));
-function addtocart(data) {
-  // var products = JSON.parse(localStorage.getItem("honeycareproduct"));
-
-  if (products) {
-    if (products[data._id]) {
-      // increase product number
-      increaseProductincart(data._id);
-    } else {
-      // add product to cart
-      data.incart = 1;
-      data.totalPrice = data.price;
-      products[data._id] = data;
-      localStorage.setItem(
-        "honeycareproduct",
-        JSON.stringify(products, null, 2)
-      );
-      numberincart();
-    }
-  } else {
-    products = {};
-    data.incart = 1;
-    data.totalPrice = parseInt(data.incart) * data.price;
-    products[data._id] = data;
-    localStorage.setItem("honeycareproduct", JSON.stringify(products, null, 2));
-    numberincart();
-  }
-}
-
-function increaseProductincart(id) {
-  products[`${id}`].incart = parseInt(products[`${id}`].incart ) + 1;
-  calculateProductprice(id);
-  localStorage.setItem("honeycareproduct", JSON.stringify(products, null, 2));
-  loadcart();
-}
-function decreaseProductincart(id) {
-  if (products[`${id}`]) {
-    products[`${id}`].incart = parseInt(products[`${id}`].incart ) - 1;
-    calculateProductprice(`${id}`);
-    localStorage.setItem("honeycareproduct", JSON.stringify(products, null, 2));
-    zeroitem(id);
-    loadcart();
-  }
-}
-
-function deleteitem(id) {
-  var ans = confirm("Do you want to remove this item");
-  if (ans === true) {
-    delete products[`${id}`];
-    localStorage.setItem("honeycareproduct", JSON.stringify(products, null, 2));
-    loadcart();   
-  } else {
-    loadcart();
-  }
-}
-function zeroitem( id) {
-  if (products[`${id}`].incart == 0) {
-    var ans = confirm("Do you want to remove this item");
-    if (ans === true) {
-      delete products[`${id}`];
-      localStorage.setItem("honeycareproduct", JSON.stringify(products, null, 2));
-      loadcart();
-    } else {
-      increaseProductincart(id);
-      loadcart();
-    }
-  }
-}
-
-
-function calculateProductprice(id) {
-  if (products[`${id}`] && products[`${id}`].incart != 0) {
-    products[`${id}`].totalPrice = parseInt(products[`${id}`].incart) * products[`${id}`].price;
-  }
-}
-function calculatetotalprice() {
-  var sumtotalprice = 0;
-  Object.values(products).forEach(product => {
-    sumtotalprice += parseInt(product.totalPrice)
-  })
-  document.getElementById("total").innerText = "N" +sumtotalprice.toLocaleString();
-}
-var testt;
-function loadcart() {
-  if (window.location.pathname == "/cart") {
-    numberincart();
-    if (products && Object.keys(products).length > 0) {
-      var cartItems = document.getElementById("cart-items");
-      cartItems.innerHTML = "";
-      Object.values(products).forEach((product) => {
-        testt = product._id;
-        calculateProductprice(product._id);
-        cartItems.innerHTML += `
-        <div class="cart-item">
-          <div class="cart-product">
-            <img src="${product.image}" alt="honeycare pharmacy ${product.name
-          }" />
-
-            <div class="">${product.name}</div>
-          </div>
-            <div class="cart-quantity">
-              <div class="qnty">
-                <span class="minus" onclick="decreaseProductincart('${product._id
-          }')">
-                  -
-                </span>
-                <input type="number" value="${product.incart}" id="cart${product._id
-          }" oninput= "inputcartnumber('${product._id}', this.value)"/>
-                <span class="add" onclick="increaseProductincart('${product._id}')">
-                  +
-                </span>
-              </div>
-            </div>
-            <div class="cart-price">${product.price.toLocaleString()}</div>
-            <div class="cart-total-price" id="carttotal${product._id
-          }" >${product.totalPrice.toLocaleString()}</div>
-            <div class="cart-delete">
-              <span class="" onclick="deleteitem('${product._id}')">x</span>
-            </div>
-        </div>`;
-      });
-    }
-    else {
-      document.getElementById(
-        "cart-items"
-      ).innerHTML = `<img src="/img/advise/advise1-small.png" alt="">`;
-    }
-    calculatetotalprice();
-  }
-}
-
-loadcart();
-
-
-function numberincart() {
-  if (products && Object.keys(products).length > 0) {
-    document.getElementsByClassName("nav-extra")[0].children[0].innerText = Object.keys(products).length;
-  } else {
-    document.getElementsByClassName("nav-extra")[0].children[0].innerText = 0;
-  }
-}
-numberincart();
-
-function inputcartnumber(id, value) {
-  if (value) {
-    if (parseInt(value) != 0) {
-      products[`${id}`].incart = parseInt(value);
-      localStorage.setItem(
-        "honeycareproduct",
-        JSON.stringify(products, null, 2)
-      );
-      loadcart();
-    } else {
-      if (parseInt(value) === 0) {
-        deleteitem(id);
-        numberincart();
-      }
-    }
-  }
-}
-
-// singleproduct add to cart
-function itemQuantity(method) {
-  var input = document.getElementById("singlenumber");
-  if (method === "increase") {
-    input.value = parseInt(input.value)+ 1;
-  } else {
-    if (input.value > 0) {
-      input.value = parseInt(input.value) - 1;
-    }
-  }
-}
-function addsingletocart(data) {
-  var input = document.getElementById("singlenumber");
-  if (input.value > 0) {
-    data.incart = parseInt(input.value);
-    addtocart(data);
-  } else {
-    input.value = 1;
-    addsingletocart(data);
-  }
+  var messageCtn = document.createElement("div");
+  var Close = document.createElement("div");
+  var btn = document.createElement("button");
   
+  suc.setAttribute("class", "message");
+  if (data.status === 208) {
+    suc.classList.add("bg-red")
+  }
+  messageCtn.setAttribute("class", "message-ctn");
+  Close.setAttribute("class", "close-message");
+
+  btn.textContent = "Close";
+
+
+  suc.setAttribute("id", `Response${Math.floor(Math.random())}`);
+  messageCtn.textContent = data.message;
+
+  Close.addEventListener("click", () => {
+    document.body.removeChild(suc);
+  })
+
+  Close.appendChild(btn);
+  suc.appendChild(messageCtn);
+  suc.appendChild(Close);
+
+  document.body.append(suc);
 }
 
+
+function Join(e) {
+  var name = e.target[0].value.trim().escape();
+  var number = e.target[1].value;
+  var school = e.target[2].value.trim().escape();
+  var skilll = e.target[3].value.trim().escape();
+  var portfolio = e.target[4].value.trim().escape();
+  var twitter = e.target[5].value.trim().escape();
+  var password = e.target[6].value.trim();
+  var confirmPassword = e.target[7].value.trim();
+
+  if (password === confirmPassword) {
+    if (
+      (name,
+      number,
+      school,
+      skilll,
+      portfolio,
+      twitter,
+      password,
+      confirmPassword)
+    ) {
+      fetch("/joinSkilll", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          name,
+          number,
+          school,
+          skilll,
+          portfolio,
+          twitter,
+          password
+        })
+      })
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            messageDiv(data);
+          })
+    }
+  } else {
+    var passError = document.getElementById("password-error");
+    e.target[6].addEventListener("keyup", () => {
+      passError.classList.add("hide");
+    });
+    passError.classList.remove("hide");
+  }
+}
+
+document.getElementById("close-list").addEventListener("click", closeList);
+function closeList() {
+  document.getElementsByClassName("search-list")[0].style.display = "none";
+  document.body.style.heportfolioht= "auto";
+  document.body.style.overflowY="scroll"
+}
+function showList() {
+  document.getElementsByClassName("search-list")[0].style.display = "block";
+  document.body.style.heportfolioht= "100vh";
+  document.body.style.overflowY="hidden"
+}
+
+function setListHead() {
+  var searchResult = JSON.parse(sessionStorage.getItem("SkilllsearchResult"));
+  var school = JSON.parse(localStorage.getItem("skilll-school"));
+
+  document.getElementById("numberofpeople").innerText = searchResult.data.length;
+  document.getElementById("keyword").innerText = searchResult.keyword;
+  document.getElementById("searchedSchool").innerText = school.school;
+}
+function loadList(data) {
+  showList();
+  setListHead();
+  var i = 0;
+  document.getElementsByClassName("search-list-list")[0].children[0].innerHTML = "";
+
+  for (i; i < data.length; i++){
+    var li = document.createElement("li");
+    var person = document.createElement("div");
+    person.setAttribute("class", "person");
+
+    var personLeft = document.createElement("div");
+    personLeft.setAttribute("class", "person-left");
+
+    var name = document.createElement("div");
+    name.setAttribute("class", "name");
+
+    name.textContent = data[i].name;
+
+    var personSkill = document.createElement("div");
+    personSkill.setAttribute("class", "person-skilll");
+    personSkill.textContent = data[i].skill;
+
+    var ul = document.createElement("ul");
+    ul.setAttribute("class", "flex");
+    var EditLi = document.createElement("li");
+    EditLi.setAttribute("class", "edit");
+    var editBtn = document.createElement("button");
+    editBtn.setAttribute("onclick", `edit('${data[i]._id}')`);
+    editBtn.textContent = "Edit";
+    EditLi.appendChild(editBtn);
+
+    var SocialLi = document.createElement("li");
+    SocialLi.setAttribute("class", "person-social flex");
+
+    var portfoliobtn = document.createElement("button");
+    var portfolioLink = document.createElement("a");
+    var portfolioImg = document.createElement("img");
+
+    // append instagram details
+    portfolioImg.setAttribute("src", "/img/social/portfolio.png");
+    portfolioLink.setAttribute("href", `${data[i].portfolio}`);
+    portfolioLink.appendChild(portfolioImg);
+    portfoliobtn.appendChild(portfolioLink);
+
+    var twitterbtn = document.createElement("button");
+    var twitterLink = document.createElement("a");
+    var twitterImg = document.createElement("img");
+
+    // append twitter details
+    twitterImg.setAttribute("src", "/img/social/twitter.png");
+    twitterLink.appendChild(twitterImg);
+    twitterLink.setAttribute("href", `${data[i].twitter}`);
+    twitterbtn.appendChild(twitterLink);
+
+    // var linkedinbtn = document.createElement("button");
+    // var linkedinLink = document.createElement("a");
+    // var linkedinImg = document.createElement("img");
+
+    // append linkedin details
+    // linkedinImg.setAttribute("src", "/img/social/linkedin.png");
+    // linkedinLink.appendChild(linkedinImg);
+    // linkedinLink.setAttribute("href", "https://linkedin.com");
+    // linkedinbtn.appendChild(linkedinLink);
+
+    SocialLi.appendChild(portfoliobtn);
+    SocialLi.appendChild(twitterbtn);
+    // SocialLi.appendChild(linkedinbtn);
+
+    ul.appendChild(EditLi);
+    ul.appendChild(SocialLi);
+
+    personLeft.appendChild(name);
+    personLeft.appendChild(personSkill);
+    personLeft.appendChild(ul);
+
+    person.appendChild(personLeft);
+    li.appendChild(person);
+
+    document
+      .getElementsByClassName("search-list-list")[0]
+      .children[0].appendChild(li);
+  }
+}
+// loadList("data");
+
+function edittoogle() {
+  if (
+    document.getElementsByClassName("edit-card")[0].style.display === "none" ||
+    document.getElementsByClassName("edit-card")[0].style.display === ""
+  ) {
+    document.getElementsByClassName("edit-card")[0].style.display = "block";
+    document.getElementsByClassName("general-cover")[0].style.display = "block";
+  } else {
+    document.getElementsByClassName("edit-card")[0].style.display = "none";
+    document.getElementsByClassName("general-cover")[0].style.display = "none";
+  }
+}
+document
+  .getElementsByClassName("close-edit")[0]
+  .addEventListener("click", edittoogle);
+document
+  .getElementsByClassName("general-cover")[0]
+  .addEventListener("click", edittoogle);
+
+function edit(id) {
+  edittoogle();
+  var searchResult = JSON.parse(sessionStorage.getItem("SkilllsearchResult"));
+  var editForm = document.getElementById("edit-form");
+  var editName = document.getElementById("edit-name");
+  var editSchool = document.getElementById("edit-school");
+  var editPortfolio = document.getElementById("edit-portfolio");
+  var editTwitter = document.getElementById("edit-twitter");
+  var editSkill = document.getElementById("edit-skilll");
+  if (searchResult) {
+    console.log(searchResult)
+    var t = searchResult.data.filter((dat) => {
+      if (dat._id === id) {
+        editName.value = dat.name;
+        editSchool.value = dat.school;
+        editPortfolio.value = dat.portfolio;
+        editTwitter.value = dat.twitter;
+        editSkill.value = dat.skill;
+        sessionStorage.setItem("myId", JSON.stringify(dat._id,null,2))
+      };
+    })
+    // for (i = 0; i < searchResult.data.length; i++){
+    //   console.log(searchResult.data[i])
+    // }
+    
+  }
+
+}
+
+document.getElementById("edit-form").addEventListener("submit", uploadChanges);
+function uploadChanges(e) {
+  e.preventDefault();
+
+  var name = e.target[0].value.trim().escape();
+  var school = e.target[1].value.trim().escape();
+  var portfolio = e.target[2].value.trim().escape();
+  var twitter = e.target[3].value.trim().escape();
+  var skill = e.target[4].value.trim().escape();
+  var password = e.target[5].value.trim();
+
+  var myId = JSON.parse(sessionStorage.getItem("myId"));
+  
+   if (
+     (name,
+     school,
+     skill,
+     portfolio,
+     twitter,
+     password, myId)
+   ) {
+     if(name.length<20 &&
+school.length<10 &&
+skill.length<60 &&
+portfolio.length<70 &&
+twitter.length<70 )
+     fetch("/UpdateSkilll", {
+       method: "POST",
+       headers: { "content-type": "application/json" },
+       body: JSON.stringify({
+         name,
+         school,
+         skill,
+         portfolio,
+         twitter,
+         password, myId
+       })
+     }).then((res) => {
+           return res.json();
+         })
+         .then((data) => {
+           messageDiv(data);
+         }).catch((err) => {
+           console.error(err);
+         })
+   }
+}
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var Port = process.env.PORT || 3001;
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var mongoose = require("mongoose");
-var multer = require("multer");
-var moment = require("moment");
+// var multer = require("multer");
+// var moment = require("moment");
 var fetch = require("node-fetch");
 var fs = require("fs");
 var bcrypt = require("bcrypt");
@@ -21,18 +21,34 @@ mongoose.connect(
   }
 );
 
+String.prototype.escape = function () {
+  var tagtoreplace = {
+    "&": "&amp;",
+    "<": "&lt",
+    ">": "&gt",
+    "=": "",
+    script: " ",
+    Script: " ",
+    '"': " ",
+    "`": " ",
+  };
+  return this.replace(/[&<>`=]/g, function (tag) {
+    return tagtoreplace[tag] || tag;
+  });
+};
+
 var public = path.join(__dirname, "./public");
 app.use(express.static(public));
 var Jsonparser = bodyParser.json();
 var Urlparser = bodyParser.urlencoded({ extended: false });
 var salt = parseInt(process.env.Salt);
-app.use(
-  session({
-    secret: "randomise it all",
-    resave: true,
-    saveUninitialized: false,
-  })
-);
+// app.use(
+//   session({
+//     secret: "randomise it all",
+//     resave: true,
+//     saveUninitialized: false,
+//   })
+// );
 // const user = require("./models/user");
 
 function auth(req, res, next) {
@@ -47,63 +63,186 @@ function auth(req, res, next) {
   });
 }
 
-// router(app, public, path, moment, auth, Jsonparser, Urlparser);
-
-app.get("/login", (req, res) => {
-  res.sendFile(public + "/html/login.html");
+// get Request
+app.get("/", (req, res) => {
+  res.sendFile(public + "/index.html");
 });
-app.post("/login", Jsonparser, async (req, res) => {
-  user.find({ name: req.body.username }).then(async (data) => {
-    if (data.length === 1) {
-      var pasw = await bcrypt.compare(req.body.password, data[0].password);
-      if (pasw === true) {
-        req.session.userId = data[0]._id;
-        res.status(200).json({ redirect: "/adams#request", status: 200 });
-      } else {
-        res.json({ message: "error", status: 208 });
+
+var Schema = mongoose.Schema;
+var SkiilllSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  school: {
+    type: String,
+    required: true,
+  },
+  skill: {
+    type: String,
+    required: true,
+  },
+  portfolio: {
+    type: String,
+    required: true,
+  },
+  twitter: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+var Skilll = new mongoose.model("skilll", SkiilllSchema);
+
+app.get("/search", Jsonparser, (req, res) => {
+  Skilll.find({
+    $and: [
+      { school: req.query.school.toLowerCase() },
+      { skill: { $regex: req.query.keyword } },
+    ],
+  }).then((data) => {
+    if (data && data.length > 0) {
+      var newData = [];
+      data.filter((dat) => {
+        var newdat = {
+          _id: dat._id,
+          name: dat.name,
+          school: dat.school,
+          skill: dat.skill,
+          portfolio: dat.portfolio,
+          twitter: dat.twitter,
+        };
+        newData.push(newdat);
+      });
+      if (newData.length > 0) {
+        res.json({ data: newData, status: 200 });
       }
     } else {
-      res.json({ message: "error", status: 208 });
+      res.json({ message: "No record Found", status: 208 });
     }
   });
 });
 
-app.post("/logout", Urlparser, (req, res) => {
-  if (req.session) {
-    req.session.destroy(function (err) {
-      if (err) {
-        return console.log(err);
+app.post("/joinSkilll", Jsonparser, (req, res) => {
+  if (
+    req.body.name &&
+    req.body.school &&
+    req.body.skilll &&
+    req.body.portfolio &&
+    req.body.twitter &&
+    req.body.password
+  ) {
+    var sec = {
+      name: req.body.name.escape(),
+      number: parseInt(req.body.number),
+    };
+    var user = {
+      name: req.body.name.escape(),
+      school: req.body.school.toLowerCase().escape(),
+      skill: req.body.skilll.escape(),
+      portfolio: req.body.portfolio.escape(),
+      twitter: req.body.twitter.escape(),
+      password: req.body.password,
+    };
+    //check if user exists, save details to database and give response
+    Skilll.findOne({
+      $or: [
+        { portfolio: { $regex: user.portfolio } },
+        { twitter: { $regex: user.twitter } },
+      ],
+    }).then(async (data) => {
+      if (data) {
+        res.json({ message: "user exist", status: 208 });
       } else {
-        res.json({ redirect: "/login", status: 200 });
+        var password = await bcrypt.hash(user.password, salt);
+        user.password = password;
+        var AddSkilll = new Skilll(user);
+        AddSkilll.save().then((data) => {
+          // console.log(data);
+          res.json({
+            message:
+              "Added, Welcome to Skilll.. To update your details, Search for your name and Click 'Edit'",
+            status: 200,
+          });
+        });
       }
     });
   }
 });
-
-app.post("/passwordchange", Jsonparser, auth, (req, res) => {
+app.post("/UpdateSkilll", Jsonparser, (req, res) => {
   // console.log(req.body);
-
-  user.find({ name: req.body.username }).then(async (data) => {
-    if (data.length === 1) {
-      var pasw = await bcrypt.compare(req.body.password, data[0].password);
-      if (pasw === true) {
-        req.session.userId = data[0]._id;
-        var newpassword = await bcrypt.hash(req.body.newpassword, salt);
-        data[0].password = newpassword;
-        data[0].name = req.body.newusername;
-        data[0].save().then(() => {
-          res
-            .status(200)
-            .json({ message: "password/username updated", status: 200 });
-        });
+  //check if user exists, Update details to database and give response
+  var id = mongodb.ObjectID(req.body.myId);
+  Skilll.findOne({ _id: id }).then(async (data) => {
+    if (data) {
+      console.log(data.password)
+      var password = await bcrypt.compare(req.body.password, data.password);
+      console.log(password)
+      if (password === true && req.body.name.length<20 && req.body.school.length<10 && req.body.skill.length<60 && req.body.portfolio.length<70 && req.body.twitter.length<70) {
+        Skilll
+          .updateOne(
+            { _id: id },
+            {
+              $set: {
+                name: req.body.name.escape(),
+                school: req.body.school.escape(),
+                skill: req.body.skill.escape(),
+                portfolio: req.body.portfolio.escape(),
+                twitter: req.body.twitter.escape(),
+              },
+            }
+          )
+          .then((data) => {
+            console.log(data);
+            res.json({ message: "Update succesful", status: 200 });
+          });
       } else {
-        res.json({ message: "error", status: 208 });
+        res.json({ message: "error occured", status: 208 });
       }
-    } else {
-      res.json({ message: "error", status: 208 });
     }
   });
 });
+
+// app.post("/logout", Urlparser, (req, res) => {
+//   if (req.session) {
+//     req.session.destroy(function (err) {
+//       if (err) {
+//         return console.log(err);
+//       } else {
+//         res.json({ redirect: "/login", status: 200 });
+//       }
+//     });
+//   }
+// });
+
+// app.post("/passwordchange", Jsonparser, auth, (req, res) => {
+//   // console.log(req.body);
+
+//   user.find({ name: req.body.username }).then(async (data) => {
+//     if (data.length === 1) {
+//       var pasw = await bcrypt.compare(req.body.password, data[0].password);
+//       if (pasw === true) {
+//         req.session.userId = data[0]._id;
+//         var newpassword = await bcrypt.hash(req.body.newpassword, salt);
+//         data[0].password = newpassword;
+//         data[0].name = req.body.newusername;
+//         data[0].save().then(() => {
+//           res
+//             .status(200)
+//             .json({ message: "password/username updated", status: 200 });
+//         });
+//       } else {
+//         res.json({ message: "error", status: 208 });
+//       }
+//     } else {
+//       res.json({ message: "error", status: 208 });
+//     }
+//   });
+// });
 
 app.listen(Port, () => {
   console.log("app started");
